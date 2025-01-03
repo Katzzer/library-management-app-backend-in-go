15c3cab97e85a6a48752c5eb05b344b60bc3162f
pavelkostal <pavelkostal@ders.cz>
2025-01-03 22:35:45 +0100

    Update main.go to dynamically set port and add /ping route
    Replaced hardcoded port with dynamic port from environment variable and added a basic /ping route for health checks. This improves flexibility and ensures the application can adapt to different deployment environments.

--- main.go
+++ main.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"go-web/routes"
+	"os"
 )
 
 func main() {
-	server := gin.Default()
-	server.LoadHTMLGlob("templates/*")
-	routes.RegisterRoutes(server)
-	err := server.Run(":8080")
-	if err != nil {
-		return
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
 	}
+
+	router.Run(":" + port)
 }
