e02bbe05ee96402bc97db04d2bea15ed521fd493
pavelkostal <pavelkostal@ders.cz>
2025-01-03 22:25:37 +0100

    Add Gin framework setup and basic routing with template
    Introduced the Gin web framework, set up basic routes, and added a sample HTML template for rendering. A `/ping` route returns JSON, and `/webpage` serves the newly created `index.html`. Updated dependencies in `go.mod` to include required packages.

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,19 @@
 package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes(server *gin.Engine) {
+	server.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	server.GET("/webpage", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+}
