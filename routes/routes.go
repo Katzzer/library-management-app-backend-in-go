c9dded306c6baffbb6fcaa823499501d46c9ac49
pavelkostal <pavelkostal@ders.cz>
2025-01-04 09:42:24 +0100

    Add event management functionality and restructure endpoints.
    Introduced CRUD operations for events with authentication. Updated database schema to include tables for events and registrations. Refactored API endpoints for improved organization and added middleware for JWT authentication.

--- routes/routes.go
+++ routes/routes.go
@@ -2,16 +2,22 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"go-web/middlewares"
 )
 
 func RegisterRoutes(server *gin.Engine) {
 
 	server.GET("/", getWelcomePage)
 
-	server.GET("/api/v1/test", testBackend)
-	server.GET("/api/v1/get-time", getTime)
+	server.GET("/api/v1/others/health-check", testBackend)
+	server.GET("/api/v1/others/get-time", getTime)
 
 	server.POST("/api/v1/registration/signup", signUp)
 	server.POST("/api/v1/registration/login", login)
+
+	authenticated := server.Group("/")
+	authenticated.Use(middlewares.Authenticate)
+
+	authenticated.GET("/api/v1/events/get-all-events", getEvents)
 
 }
