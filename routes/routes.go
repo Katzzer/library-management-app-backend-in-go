a8202da0b8276655fcce157d141619c3ab70d9ce
katzz <info@pavelkostal.com>
2025-01-11 20:53:51 +0100

    "Fix CORS configuration by removing trailing slashes.
    Removed trailing slashes from allowed origins in the CORS settings. This ensures proper matching of incoming requests and prevents potential issues with origin validation."

--- routes/routes.go
+++ routes/routes.go
@@ -13,7 +13,7 @@ func RegisterRoutes(server *gin.Engine) {
 	startTime = time.Now()
 
 	server.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:4200", "http://localhost:3000", "https://library-management-app-frontend.pavelkostal.com/", "https://library-management-app-frontend-angular.pavelkostal.com/"}, // Only allow these origins
+		AllowOrigins:     []string{"http://localhost:4200", "http://localhost:3000", "https://library-management-app-frontend.pavelkostal.com", "https://library-management-app-frontend-angular.pavelkostal.com"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
